97880b759c11010a2dc863ee11506d880da380e0
magfon <magnus.hfondenes@gmail.com>
2024-09-25 19:23:32 +0200

    a b matriser

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,27 +2,50 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
+	public static void main(String[] args) {
+		int[][] tab = { { 3, 2, 1, 7 },
+						{ 9, 11, 5, 4 }, 
+						{ 6, 0, 13, 17 }, 
+						{ 7, 21, 14, 15 } };
+
+		//skrivUt(tab);
+System.out.print(tilStreng(tab));
+	}
+
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		System.out.print("[");
+		for (int[] rad : matrise) {
+			for (int i : rad) {
+				System.out.print(i + ", ");
+			}
+		}
+		System.out.print("]");
+
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		StringBuilder sb = new StringBuilder();
+		for (int[] rad : matrise) {
+			sb.append("\n");
+			for (int i : rad) {
+				sb.append(i + " ");
+			}
+		}
+		String tableAsString =  sb.toString();
+
+		return tableAsString;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
+
 		// TODO
 		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
 	}
 
 	// d)
@@ -30,16 +53,16 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+
 	}
-	
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
 		// TODO
 
 		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+
 	}
 
 	// f)
@@ -47,6 +70,6 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+
 	}
 }
